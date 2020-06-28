2b98d08625f61e78cf084b85f983bca92fd01b09
Crosstyan <crosstyan@outlook.com>
2020-06-28 13:21:32 +0800

    change to index.ts

--- src/index.js
+++ src/index.js
@@ -8,36 +8,56 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = __importDefault(require("ws"));
 const mongodb_1 = require("mongodb");
-const typegoose_1 = require("typegoose");
-const cfg = require("./config.json");
-class PostAnother {
+const mongoose_1 = __importDefault(require("mongoose"));
+const typegoose_1 = require("@typegoose/typegoose");
+const ajv_1 = __importDefault(require("ajv"));
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const url_1 = __importDefault(require("url"));
+//use import json to keep type check
+const config_json_1 = __importDefault(require("./config.json"));
+//use typescript-json-schema (https://github.com/YousefED/typescript-json-schema)
+//to compile the interface to JSON Schema
+//IDK why typescript doesn't let me to use import
+//Anyway type checking is not needed in schema
+const postSchema = require('./PostSchema.json');
+class Post {
 }
 __decorate([
     typegoose_1.prop(),
     __metadata("design:type", mongodb_1.ObjectID)
-], PostAnother.prototype, "id", void 0);
+], Post.prototype, "id", void 0);
 __decorate([
-    typegoose_1.prop(),
-    __metadata("design:type", mongodb_1.ObjectID)
-], PostAnother.prototype, "threadId", void 0);
+    typegoose_1.prop({ required: true }),
+    __metadata("design:type", String)
+], Post.prototype, "nickname", void 0);
+__decorate([
+    typegoose_1.prop({ required: true }),
+    __metadata("design:type", String)
+], Post.prototype, "content", void 0);
 __decorate([
     typegoose_1.prop(),
     __metadata("design:type", String)
-], PostAnother.prototype, "nickname", void 0);
+], Post.prototype, "token", void 0);
 __decorate([
-    typegoose_1.prop(),
+    typegoose_1.prop({ required: true }),
     __metadata("design:type", String)
-], PostAnother.prototype, "content", void 0);
-__decorate([
-    typegoose_1.prop(),
-    __metadata("design:type", String)
-], PostAnother.prototype, "token", void 0);
+], Post.prototype, "threadId", void 0);
 class Thread {
 }
 __decorate([
@@ -61,7 +81,7 @@ __decorate([
     __metadata("design:type", String)
 ], Thread.prototype, "creatorToken", void 0);
 __decorate([
-    typegoose_1.prop({ ref: PostAnother }),
+    typegoose_1.prop({ ref: Post }),
     __metadata("design:type", mongodb_1.ObjectID)
 ], Thread.prototype, "postList", void 0);
 __decorate([
@@ -96,18 +116,232 @@ __decorate([
     typegoose_1.prop(),
     __metadata("design:type", String)
 ], User.prototype, "password", void 0);
-class Tag {
+class ErrorMsg {
+    constructor() {
+        this.errorMap = {};
+    }
+    toString(code) {
+        if (this.errorMap[code] != undefined) {
+            const msg = {
+                code: code,
+                description: this.errorMap[code]
+            };
+            return JSON.stringify(msg);
+        }
+        else {
+            const msg = {
+                code: 622,
+                description: "Unknown error"
+            };
+            return JSON.stringify(msg);
+        }
+    }
 }
-__decorate([
-    typegoose_1.prop(),
-    __metadata("design:type", String)
-], Tag.prototype, "name", void 0);
-class BoardSession {
+class WsHub {
+    constructor() {
+        this.wsSessionGroupMap = {};
+    }
+    //wsSessionGroupMap:Map<string,WsSessionGroup> = new Map
+    addSession(groupName, session) {
+        if (this.wsSessionGroupMap[groupName] == undefined) {
+            const sessionGroup = new ThreadSessionGroup;
+            sessionGroup.name = groupName;
+            sessionGroup.wsSessions.push(session);
+            this.wsSessionGroupMap[groupName] = sessionGroup;
+            //If you wanna use uuid map
+            // const uid = uuid()
+            // this.wsSessionGroupMap[groupName].wsSessions[uid]=session
+            console.log(`New group "${groupName}" created`);
+        }
+        else {
+            this.wsSessionGroupMap[groupName].wsSessions.push(session);
+            console.log(`Old group "${groupName}" connected`);
+        }
+    }
+    deleteSession(groupName, session) {
+        const sessionList = this.wsSessionGroupMap[groupName].wsSessions;
+        sessionList.splice(sessionList.indexOf(session), 1);
+        console.log(`A member from group "${groupName}" disconnected`);
+        if (sessionList.length == 0) {
+            delete this.wsSessionGroupMap[groupName];
+            console.log(`Delete empty group "${groupName}"`);
+        }
+    }
+    broadcastGroup(groupName, message) {
+        const targetGroup = this.wsSessionGroupMap[groupName];
+        targetGroup === null || targetGroup === void 0 ? void 0 : targetGroup.wsSessions.forEach((session) => {
+            if (session.readyState === ws_1.default.OPEN) {
+                session.send(message);
+            }
+        });
+        console.log(`"${message}" from "${groupName}"`);
+    }
 }
+class ThreadSessionGroup {
+    constructor() {
+        this.wsSessions = [];
+    }
+}
+function parsePost(wsData) {
+    const wsDataStr = wsData.toString();
+    const ajv = new ajv_1.default({ allErrors: true });
+    try {
+        const wsDataJson = JSON.parse(wsDataStr);
+        const isValid = ajv.validate(postSchema, wsDataJson);
+        if (isValid) {
+            return wsDataJson;
+        }
+        else {
+            return null;
+        }
+    }
+    catch (_a) {
+        return null;
+    }
+}
+//Express Web Server
+//const EXPRESS_PORT=8080
+const app = express_1.default();
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+app.get('/board', (req, res) => {
+    //Get board name
+});
+app.get('/board/:boardName', (req, res) => {
+    //Get all the thread in board
+    try {
+        PostModel.find().lean().exec((err, threads) => {
+            res.json(threads);
+        });
+    }
+    catch (_a) {
+        res.json(errorMsg.toString(404));
+    }
+});
+app.get('/board/:boardName/:threadId', (req, res) => {
+    //Get all the post in thread
+    try {
+    }
+    catch (_a) {
+        res.json(errorMsg.toString(404));
+    }
+});
+//Error Parse
+const errorMsg = new ErrorMsg;
+errorMsg.errorMap = {
+    400: "Bad request",
+    404: "Not found",
+    1003: "Params error",
+    500: "Internal server error",
+};
+//MongoDB
+const mongoURL = `mongodb://${config_json_1.default.database.hostname}:${config_json_1.default.database.port}/${config_json_1.default.database.name}`;
+const PostModel = typegoose_1.getModelForClass(Post);
+mongoose_1.default.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => {
+    console.log(err);
+});
+const db = mongoose_1.default.connection;
+db.on('error', console.error.bind(console, 'error when connecting to mongodb'));
+db.once('open', () => {
+    console.log("MongoDB is working");
+});
+//Server
+const PORT = config_json_1.default.server.port;
+const httpServer = http_1.default.createServer();
+httpServer.on('request', app); //app=express() is a callback function
+httpServer.on('upgrade', (req, socket, head) => {
+    const pathname = url_1.default.parse(req.url).pathname;
+    if (pathname === '/board') {
+        boardWs.handleUpgrade(req, socket, head, (webSocketConn) => {
+            boardWs.emit('connection', webSocketConn, req);
+        });
+    }
+    else if (pathname === '/thread') {
+        threadWs.handleUpgrade(req, socket, head, (webSocketConn) => {
+            threadWs.emit('connection', webSocketConn, req);
+        });
+    }
+    else {
+        socket.destroy();
+    }
+});
+httpServer.listen(PORT, () => {
+    console.log(`Listening ${PORT}`);
+});
+//Websocket Board Server
+//Stupid you can't do that! A http server can only bind a websocket server. 
+//Actually you can use node http server. 
+const boardHub = new WsHub;
 const boardWs = new ws_1.default.Server({
-    port: cfg.server.port,
-    path: "/board",
+    noServer: true
 });
-boardWs.on('connection', (socket) => {
+boardWs.on('connection', (webSocketConn, req) => {
+    webSocketConn.send("Hello World");
 });
+//Websocket Thread Server
+const threadHub = new WsHub;
+const threadWs = new ws_1.default.Server({
+    noServer: true
+});
+threadWs.on('connection', (webSocketConn, req) => {
+    threadApp(webSocketConn, req);
+});
+function threadApp(webSocketConn, req) {
+    var _a;
+    return __awaiter(this, void 0, void 0, function* () {
+        //const urlParam = querystring.decode(urlParamMap!)
+        try {
+            const urlParamMap = url_1.default.parse(req.url, true);
+            const sessionNameInURL = (_a = urlParamMap.query["name"]) === null || _a === void 0 ? void 0 : _a.toString();
+            if (sessionNameInURL != undefined) {
+                threadHub.addSession(sessionNameInURL, webSocketConn);
+                webSocketConn.on('message', (message) => __awaiter(this, void 0, void 0, function* () {
+                    onPost(webSocketConn, message, sessionNameInURL);
+                }));
+                webSocketConn.on('close', () => {
+                    threadHub.deleteSession(sessionNameInURL, webSocketConn);
+                });
+            }
+            else {
+                throw new Error('Params error');
+            }
+        }
+        catch (_b) {
+            webSocketConn.send(errorMsg.toString(1003));
+            webSocketConn.close(1003, "Params error");
+        }
+    });
+}
+function onPost(webSocketConn, message, sessionGroupName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const messageParsed = parsePost(message);
+        if (messageParsed != null) {
+            const savedMsg = yield savePost(messageParsed);
+            if (savedMsg != null) {
+                const messageStr = JSON.stringify(savedMsg);
+                threadHub.broadcastGroup(sessionGroupName, messageStr);
+            }
+            else {
+                webSocketConn.send(errorMsg.toString(500));
+            }
+        }
+        else {
+            webSocketConn.send(errorMsg.toString(400));
+        }
+    });
+}
+function savePost(messageParsed) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let msgModel = new PostModel(messageParsed);
+            const obj = yield msgModel.save();
+            console.log("Post created");
+            return obj;
+        }
+        catch (_a) {
+            console.log("Post creating failed");
+        }
+    });
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,56 @@
-import express from "express"
-import * as http from "http"
 import WebSocket from 'ws'
-import * as path from 'path'
 import mongodb, { ObjectID } from 'mongodb'
-import * as mongoose from 'mongoose'
-import { type } from "os"
-import { prop, Typegoose, ModelType, InstanceType } from 'typegoose'
+import mongoose from 'mongoose'
+import { prop, getModelForClass } from '@typegoose/typegoose'
+import * as querystring from 'querystring'
+import { v4 as uuid } from 'uuid'
+import Ajv from 'ajv'
+import express from 'express'
+import http, { Server } from 'http'
+import url from 'url'
+import { Socket } from 'net'
+//use import json to keep type check
+import cfg from './config.json'
 
-const cfg = require("./config.json")
+//use typescript-json-schema (https://github.com/YousefED/typescript-json-schema)
+//to compile the interface to JSON Schema
+//IDK why typescript doesn't let me to use import
+//Anyway type checking is not needed in schema
+const postSchema = require('./PostSchema.json') 
 
-class PostAnother {
-  @prop()
-  id: ObjectID
-  @prop()
-  threadId: ObjectID
-  @prop()
+//Connect to ws://127.0.0.1:8081/thread?name=yourGroupname
+//to test the server
+
+/**
+ * @TJS-additionalProperties false
+ */
+export interface PostInterface {
+  id?: ObjectID
   nickname: string
+  content:string
+  token?: string
+  threadId:string
+}
+
+class Post implements PostInterface{
   @prop()
-  content: string
+  public id?: ObjectID
+
+  @prop({required:true})
+  public nickname!: string
+
+  @prop({required:true})
+  public content!:string
+  
   @prop()
-  token:string
+  public token?: string
+  
+  @prop({required:true})
+  public threadId!:string
+}
+
+interface ThreadInterface{
+
 }
 class Thread{
   @prop()
@@ -32,10 +63,10 @@ class Thread{
   creator: string
   @prop()
   creatorToken: string
-  @prop({ref:PostAnother})
+  @prop({ref:Post})
   postList: ObjectID
   @prop()
-  tag:Tag[]
+  tag:string[]
 }
 class Board{
   @prop()
@@ -53,25 +84,229 @@ class User{
   @prop()
   password: string
 }
-class Tag{
-  @prop()
-  name:string
+
+class ErrorMsg {
+  errorMap: Record<number, string> = {}
+  toString(code: number): string {
+    if (this.errorMap[code] != undefined) {
+        const msg={
+          code: code,
+          description:this.errorMap[code]
+        }
+        return JSON.stringify(msg)
+    } else {
+      const msg={
+        code: 622,
+        description:"Unknown error"
+      }
+      return JSON.stringify(msg)
+      }
+    }
 }
 
-interface WsSession{
-  id: ObjectID
-  wsConnection:WebSocket[]
+interface WsSessionGroup{
+  id?: ObjectID
+  name?:string
+  wsSessions: WebSocket[]
+  //Or use a map with uuid
+  //wsSessions:Record<string,WebSocket>
 }
-class BoardSession implements WsSession{
-  id: ObjectID
-  wsConnection:WebSocket[]
+class WsHub{
+  wsSessionGroupMap: Record<string, WsSessionGroup> = {}
+  //wsSessionGroupMap:Map<string,WsSessionGroup> = new Map
+  addSession(groupName:string,session:WebSocket) {
+    if (this.wsSessionGroupMap[groupName]==undefined){
+      const sessionGroup = new ThreadSessionGroup
+      sessionGroup.name = groupName
+      sessionGroup.wsSessions.push(session)
+      this.wsSessionGroupMap[groupName] = sessionGroup
+      //If you wanna use uuid map
+      // const uid = uuid()
+      // this.wsSessionGroupMap[groupName].wsSessions[uid]=session
+      console.log(`New group "${groupName}" created`)
+    }
+    else {
+      this.wsSessionGroupMap[groupName].wsSessions.push(session)
+      console.log(`Old group "${groupName}" connected`)
+    }
+  }
+  deleteSession(groupName:string,session:WebSocket):void {
+    const sessionList = this.wsSessionGroupMap[groupName].wsSessions
+    sessionList.splice(sessionList.indexOf(session), 1)
+    console.log(`A member from group "${groupName}" disconnected`)
+    if (sessionList.length == 0) {
+      delete this.wsSessionGroupMap[groupName]
+      console.log(`Delete empty group "${groupName}"`)
+    }
+  }
+  broadcastGroup(groupName:string, message: WebSocket.Data):void {
+    const targetGroup = this.wsSessionGroupMap[groupName]
+    targetGroup?.wsSessions.forEach((session) => {
+      if (session.readyState === WebSocket.OPEN) {
+        session.send(message)
+      }
+    })
+    console.log(`"${message}" from "${groupName}"`)
+  }
+}
+class ThreadSessionGroup implements WsSessionGroup{
+  id?: ObjectID
+  name?:string
+  wsSessions: WebSocket[] =[]
 }
 
+function parsePost(wsData:WebSocket.Data) :Post|null{
+  const wsDataStr = wsData.toString()
+  const ajv = new Ajv({allErrors:true})
+  try {
+    const wsDataJson = JSON.parse(wsDataStr)
+    const isValid = ajv.validate(postSchema,wsDataJson)
+    if (isValid) {
+      return wsDataJson as Post
+    } else {
+      return null
+    }
+  } catch{
+    return null
+  }
+}
+
+//Express Web Server
+//const EXPRESS_PORT=8080
+const app = express()
+app.get('/', (req,res) => {
+  res.send("hello world")
+})
+app.get('/board', (req, res) => {
+  //Get board name
+})
+app.get('/board/:boardName', (req, res) => {
+  //Get all the thread in board
+  try {
+    PostModel.find().lean().exec((err, threads) => {
+      res.json(threads)
+    })
+  } catch{
+    res.json(errorMsg.toString(404))
+  }
+})
+app.get('/board/:boardName/:threadId', (req, res) => {
+  //Get all the post in thread
+  try {
+    
+  } catch{
+    res.json(errorMsg.toString(404))
+  }
+})
+
+//Error Parse
+const errorMsg = new ErrorMsg
+errorMsg.errorMap = {
+  400: "Bad request",
+  404: "Not found",
+  1003: "Params error",
+  500:"Internal server error",
+}
+
+//MongoDB
+const mongoURL = `mongodb://${cfg.database.hostname}:${cfg.database.port}/${cfg.database.name}`
+const PostModel=getModelForClass(Post)
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err => {
+  console.log(err)
+})
+const db=mongoose.connection
+db.on('error', console.error.bind(console, 'error when connecting to mongodb'))
+db.once('open', () => { //Do something when db connection on. 
+  console.log("MongoDB is working")
+})
+
+//Server
+const PORT=cfg.server.port
+const httpServer = http.createServer()
+httpServer.on('request', app) //app=express() is a callback function
+httpServer.on('upgrade', (req:http.IncomingMessage, socket:Socket, head:Buffer) => {
+  const pathname = url.parse(req.url!).pathname;
+  if (pathname === '/board') {
+    boardWs.handleUpgrade(req, socket, head, (webSocketConn) => {
+      boardWs.emit('connection', webSocketConn,req)
+    });
+  } else if (pathname === '/thread') {
+    threadWs.handleUpgrade(req, socket, head, (webSocketConn) => {
+      threadWs.emit('connection', webSocketConn,req);
+    });
+  } else {
+    socket.destroy();
+  }
+})
+httpServer.listen(PORT, () => {
+  console.log(`Listening ${PORT}`)
+})
+
+
+//Websocket Board Server
+//Stupid you can't do that! A http server can only bind a websocket server. 
+//Actually you can use node http server. 
+const boardHub = new WsHub
 const boardWs = new WebSocket.Server({
-  port: cfg.server.port,
-  path: "/board",
+  noServer:true
+})
+boardWs.on('connection', (webSocketConn, req) => {
+  webSocketConn.send("Hello World")
 })
 
-boardWs.on('connection', (socket) => {
-  
+//Websocket Thread Server
+const threadHub = new WsHub
+const threadWs = new WebSocket.Server({
+  noServer:true
 })
+
+threadWs.on('connection', (webSocketConn, req) => {
+  threadApp(webSocketConn,req)
+})
+async function threadApp(webSocketConn: WebSocket, req: http.IncomingMessage) {
+  //const urlParam = querystring.decode(urlParamMap!)
+  try {
+    const urlParamMap = url.parse(req.url!, true)
+    const sessionNameInURL = urlParamMap.query["name"]?.toString()
+    if (sessionNameInURL != undefined) {
+      threadHub.addSession(sessionNameInURL,webSocketConn)
+      webSocketConn.on('message', async (message) => { //Must add async here to keep the structure async
+        onPost(webSocketConn,message,sessionNameInURL)
+      })
+      webSocketConn.on('close', () => {
+        threadHub.deleteSession(sessionNameInURL, webSocketConn)
+      })
+    } else {
+      throw new Error('Params error')
+    }
+  } catch{
+    webSocketConn.send(errorMsg.toString(1003))
+    webSocketConn.close(1003,"Params error")
+  }
+}
+
+async function onPost(webSocketConn:WebSocket,message:WebSocket.Data,sessionGroupName:string) {
+  const messageParsed = parsePost(message)
+  if (messageParsed != null) {      
+    const savedMsg = await savePost(messageParsed)
+    if (savedMsg != null) {
+      const messageStr = JSON.stringify(savedMsg) 
+      threadHub.broadcastGroup(sessionGroupName,messageStr)
+    } else {
+      webSocketConn.send(errorMsg.toString(500))
+    }
+  } else {
+    webSocketConn.send(errorMsg.toString(400))
+  }
+}
+
+async function savePost(messageParsed:Post) {
+  try {
+    let msgModel = new PostModel(messageParsed)
+    const obj = await msgModel.save()
+    console.log("Post created")
+    return obj
+  } catch{
+    console.log("Post creating failed")
+  }
+}
